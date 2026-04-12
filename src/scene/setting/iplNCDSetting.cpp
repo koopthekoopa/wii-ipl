7e8069eadd9839b674645c278a5fbe6e5c809109
kr1viah <kravpolet@gmail.com>
2026-04-12 19:40:29 +0200

    make init actually readable

--- src/scene/setting/iplNCDSetting.cpp
+++ src/scene/setting/iplNCDSetting.cpp
@@ -5,14 +5,10 @@
 namespace ipl {
     namespace ncd {
         int NCDSetting::init() {
-            s32 sVar1;
-            int iVar2;
-
-            memset(&mConfig, 0, 0x1b5c);
-            sVar1 = NCDReadConfig((NCDConfig*)&mConfig);
-            OSReport("NCDReadConfig: %d\n", sVar1);
-            iVar2 = makeMacAddr();
-            return iVar2;
+            memset(&mConfig, 0, 0x1b5c);              // initialize config to all 0s (?)
+            int status = NCDReadConfig(&mConfig);     // read config
+            OSReport("NCDReadConfig: %d\n", status);  // log status
+            return makeMacAddr();                     // return mac address
         }
 
         void NCDSetting::initSetID(unsigned short setID) {
