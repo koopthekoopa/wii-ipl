bc42660825e7723762930165477c565a0278ece5
kr1viah <kravpolet@gmail.com>
2026-04-14 16:59:26 +0200

    convert16toASCII() 98.85%

--- src/scene/setting/iplNCDSetting.cpp
+++ src/scene/setting/iplNCDSetting.cpp
@@ -24,6 +24,46 @@ namespace ipl {
             mID = setID;
         }
 
+        // feels wrong
+        void NCDSetting::checkThisFlag() {
+            checkFlag(mID);
+        }
+
+        undefined4 NCDSetting::checkFlag(int id) {
+            int flags = mConfig.profiles[id].flags;
+            if ((flags & IPL_NCD_SETTING_DHCP_FLAG) != 0) {
+                return 1;
+            }
+            if (mConfig.profiles[id].netif.wireless.config.manual.ssidLength == 0 && mConfig.profiles[id].netif.wireless.configMethod == 0 &&
+                mConfig.profiles[id].netif.wireless.config.manual.privacy.mode == 0) {
+                return 0;
+            }
+            int bVar1 = mConfig.profiles[id].netif.wireless.configMethod;
+            if (bVar1 != 2) {
+                if (bVar1 >= 0) {
+                    if (bVar1 > 4) {
+                        if (bVar1 > 0) {
+                            return 1;
+                        }
+                        return 2;
+                    }
+                    return 3;
+                }
+                return 4;
+            }
+            return 5;
+        }
+
+        // TODO: ask about why ghidra shows undefined4 for this. its clearly meant to be a bool
+        undefined4 NCDSetting::checkAllFlag() {
+            for (int i = 0; i < 3; i++) {
+                if (checkFlag(i) & 0xff) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         u8 NCDSetting::checkConnectTestFlag() {
             return (mConfig.profiles[mID].flags) >> IPL_NCD_SETTING_CONNECT_TEST_FLAG & 1;
         }
@@ -89,6 +129,32 @@ namespace ipl {
         unsigned int NCDSetting::checkChangeEnable() {
             u8 configMethod = mConfig.profiles[mID].netif.wireless.configMethod;
             return (1 - ((u8)configMethod + 0xff & 0xff)) >> (u8)0x1f;
+        }
+
+        undefined4 NCDSetting::convert16toASCII(char param_1, char param_2, unsigned char* param_3) {
+            int ret = true;
+
+            if (param_1 >= '0' && param_1 <= '9') {
+                *param_3 = (param_1 - '0') << 4;
+            } else if (param_1 >= 'A' && param_1 <= 'F') {
+                *param_3 = (param_1 - 'A' + 10) << 4;
+            } else if (param_1 >= 'a' && param_1 <= 'f') {
+                *param_3 = (param_1 - 'a' + 10) << 4;
+            } else {
+                ret = false;
+            }
+
+            if (param_2 >= '0' && param_2 <= '9') {
+                *param_3 = *param_3 + ((param_2 - '0') << 24);
+            } else if (param_2 >= 'A' && param_2 <= 'F') {
+                *param_3 = *param_3 + ((param_2 - 'A' + 10) << 24);
+            } else if (param_2 >= 'a' && param_2 <= 'f') {
+                *param_3 = *param_3 + ((param_2 - 'a' + 10) << 24);
+            } else {
+                ret = false;
+            }
+
+            return ret;
         }
     }  // namespace ncd
 }  // namespace ipl
