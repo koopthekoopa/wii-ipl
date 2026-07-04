a50069a580cf56e55683da041182c1ad956007f2
Tim Schneeberger <tim.schneeberger@outlook.de>
2026-07-04 06:55:44 +0200

    improve TMCJPEGDEC_make_huffdec

--- libs/RVLMiddleware/TMC_JPEG/src/buffer/mkhdec3.c
+++ libs/RVLMiddleware/TMC_JPEG/src/buffer/mkhdec3.c
@@ -1,5 +1,153 @@
 #include <tmc_jpeg_internal.h>
 
+s32 TMCJPEGDEC_make_huffdec(const u8* dht, const u8* tb, TMCHuffParam* hp)
+{
+    u32 huffVal[256];
+    u32 huffCount[256];
+    u32* huffTable;
+    u32* valptr;
+    u32* destTable;
+    u32 count;
+    u32 one;
+    u32* hc;
+    s32 sym_idx;
+    u32 remaining;
+    u32 bit_len;
+    const u8* bits;
+
+    count = hp->count;
+    huffTable = (u32*)hp->huffTable;
+    valptr = (u32*)hp->valptr;
+    destTable = (u32*)hp->destTable;
+
+    hc = huffCount;
+    sym_idx = 0;
+    remaining = 1;
+    bit_len = 1;
+    bits = dht + 1;
+
+    for (;;)
+    {
+        u32 cnt;
+
+        cnt = *bits;
+        if (remaining > cnt)
+        {
+            bit_len++;
+            remaining = 1;
+            bits++;
+            if (bit_len > 16)
+            {
+                huffCount[sym_idx] = 0;
+                break;
+            }
+        }
+        else
+        {
+            hc[0] = bit_len;
+            hc++;
+            sym_idx++;
+            if (sym_idx > (s32)count)
+                return -0x40;
+            remaining++;
+        }
+    }
+
+    {
+        u32* hv;
+        s32 i;
+        u32 sv_idx;
+        u32 cur;
+
+        hv = huffVal;
+        i = 0;
+        sv_idx = 0;
+        one = 1;
+        cur = huffCount[0] & 0xFF;
+
+        while (cur != 0)
+        {
+            u32* hcp;
+
+            hcp = &huffCount[i];
+            while (i < (s32)count && (*hcp & 0xFF) == cur)
+            {
+                hv[0] = sv_idx & 0xFFFF;
+                hv++;
+                sv_idx++;
+                i++;
+                hcp++;
+            }
+            if (sv_idx > (one << cur))
+                return -0x40;
+            sv_idx <<= 1;
+            cur = huffCount[i] & 0xFF;
+        }
+    }
+
+    memset(valptr, 0, 0x44);
+
+    {
+        s32 i;
+
+        for (i = 0; i < (s32)count; i++)
+        {
+            u32 bl;
+
+            bl = huffCount[i];
+            if (bl <= 16)
+            {
+                valptr[bl] = (i << 16) | (huffVal[i] & 0xFFFF);
+            }
+        }
+    }
+
+    {
+        const u8* bits_ptr;
+        u32* hv_ptr;
+        s32 sym;
+        u32 bl;
+        u32 n;
+        u32 step;
+        u32 cnt;
+
+        bits_ptr = dht + 1;
+        hv_ptr = huffVal;
+        sym = 0;
+
+        for (bl = 1; bl <= 8; bl++)
+        {
+            cnt = *bits_ptr;
+            step = 1 << (8 - bl);
+
+            for (n = 0; n < cnt; n++)
+            {
+                u32 code;
+                u32 base;
+                s32 kk;
+
+                code = *hv_ptr;
+                hv_ptr++;
+                base = code << (8 - bl);
+
+                for (kk = 0; kk < (s32)step; kk++)
+                {
+                    ((u16*)huffTable)[(base + kk) * 2] = bl;
+                    ((u16*)huffTable)[(base + kk) * 2 + 1] = tb[sym];
+                }
+
+                sym++;
+            }
+
+            bits_ptr++;
+        }
+    }
+
+    memcpy(destTable, tb, count);
+
+    return 0;
+}
+
 void TMCJPEGDEC_set_HuffmanTable(TMCHuffTblSet* tbl, s32 tblType, s32 tblID,
                                  TMCJpegDecWork* work)
 {
@@ -70,151 +218,4 @@ ac1:
     memset(work->mMaxCode_AC1, 0, 0x100);
     memset(work->mValPtr_AC1, 0, 0x44);
     return;
-}
-
-s32 TMCJPEGDEC_make_huffdec(const u8* dht, const u8* tb, TMCHuffParam* hp)
-{
-    u32 huffVal[256];
-    u32 huffCount[256];
-    u32* huffTable;
-    u32* valptr;
-    u32* destTable;
-    s32 count;
-
-    {
-        u32* hc;
-        s32 sym_idx;
-        u32 remaining;
-        u32 bit_len;
-        u32 cnt;
-        const u8* bits;
-
-        count = hp->count;
-        hc = huffCount;
-        sym_idx = 0;
-        remaining = 1;
-        bit_len = 1;
-        bits = dht + 1;
-
-        for (;;)
-        {
-            cnt = *bits;
-            if (remaining <= cnt)
-            {
-                sym_idx++;
-                *hc = bit_len;
-                hc++;
-                if (sym_idx > count)
-                    return -0x40;
-                remaining++;
-            }
-            else
-            {
-                bit_len++;
-                remaining = 1;
-                bits++;
-                if (bit_len > 16)
-                {
-                    huffCount[sym_idx] = 0;
-                    break;
-                }
-            }
-        }
-    }
-
-    huffTable = (u32*)hp->huffTable;
-    valptr = (u32*)hp->valptr;
-    destTable = (u32*)hp->destTable;
-
-    {
-        s32 i;
-        u32 sv_idx;
-        u32 code_counter;
-        u32 cur;
-
-        sv_idx = 0;
-        code_counter = 0;
-        i = 0;
-
-        while (i < count)
-        {
-            cur = (u8)huffCount[i];
-            if (cur == 0)
-                break;
-
-            while (i < count && (u8)huffCount[i] == cur)
-            {
-                huffVal[sv_idx] = (u16)code_counter;
-                sv_idx++;
-                code_counter++;
-                i++;
-            }
-
-            if (sv_idx > (1u << cur))
-                return -0x40;
-            code_counter = (code_counter + 1) << 1;
-        }
-    }
-
-    memset(valptr, 0, 0x44);
-
-    {
-        s32 i;
-        u32 bl;
-        u32 cd;
-
-        for (i = 0; i < count; i++)
-        {
-            bl = huffCount[i];
-            cd = huffVal[i];
-            if (bl <= 16)
-            {
-                valptr[bl] = (i << 16) | cd;
-            }
-        }
-    }
-
-    {
-        const u8* bits_ptr;
-        u32* hv_ptr;
-        s32 sym_idx;
-        u32 bit_len;
-        u32 step;
-        s32 n;
-        s32 kk;
-        u32 cnt;
-        u32 code;
-        u32 base;
-
-        sym_idx = 0;
-        bits_ptr = dht + 1;
-        hv_ptr = huffVal;
-
-        for (bit_len = 1; bit_len <= 8; bit_len++)
-        {
-            cnt = *bits_ptr;
-            step = 1 << (8 - bit_len);
-
-            for (n = 0; n < (s32)cnt; n++)
-            {
-                code = *hv_ptr;
-                base = code << (8 - bit_len);
-
-                for (kk = 0; kk < (s32)step; kk++)
-                {
-                    ((u16*)huffTable)[(base + kk) * 2] = (u16)bit_len;
-                    ((u16*)huffTable)[(base + kk) * 2 + 1] = (u16)tb[sym_idx];
-                }
-
-                hv_ptr++;
-                sym_idx++;
-            }
-
-            bits_ptr++;
-        }
-    }
-
-    memcpy(destTable, tb, count);
-
-    return 0;
 }
