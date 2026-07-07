3c7ddd567430aea91b67a6c00e0e563d2b510252
Tim Schneeberger <tim.schneeberger@outlook.de>
2026-07-07 23:51:30 +0200

    Improve TMCJPEGDEC_IdctBlock4x4_Col

--- libs/RVLMiddleware/TMC_JPEG/src/reschange/idct_resolution_change_var.c
+++ libs/RVLMiddleware/TMC_JPEG/src/reschange/idct_resolution_change_var.c
@@ -125,74 +125,65 @@ void TMCJPEGDEC_IdctBlock1x1(s32* block, u8* conv_row_ptr, u16 pitch, s32 zigzag
     conv_row_ptr[0] = Clamp_U8((in >> 11) + 0x80);
 }
 
-void TMCJPEGDEC_IdctBlock4x4_Col(s32* block, u8* conv_row_ptr, u16 pitch, s32 zigzag) {
+void TMCJPEGDEC_IdctBlock4x4_Col(s32* sp, u8* conv_row_ptr, u16 pitch, s32 zigzag) {
     s32 tmp[64];
-    s32* sp;
-    s32* dp;
+    s32* src = sp + 24;
+    s32* dst = tmp + 24;
     s32 i;
     s32 a, b, c, d;
-    s32 evsum, evdiff, rot, oddrot;
-
-    sp = block + 24;
-    dp = tmp + 24;
+    s32 evsum, evdiff, oddsum, odddiff, rot, oddrot;
 
     for (i = 0; i < 2; i++) {
-        a = sp[0];
-        b = sp[1];
-        c = sp[2];
-        d = sp[3];
-        evsum = a + c;
-        evdiff = a - c;
-        rot = (b - d) * 0xB5 >> 8;
-        oddrot = rot + b + d;
-        dp[0] = evsum + oddrot;
-        dp[1] = evdiff + rot;
-        dp[2] = evdiff - rot;
-        dp[3] = evsum - oddrot;
+        s32 a0 = src[0], b0 = src[1], c0 = src[2], d0 = src[3];
+        s32 a1 = src[-8], b1 = src[-7], c1 = src[-6], d1 = src[-5];
 
-        a = sp[-8];
-        b = sp[-7];
-        c = sp[-6];
-        d = sp[-5];
-        evsum = a + c;
-        evdiff = a - c;
-        rot = (b - d) * 0xB5 >> 8;
-        oddrot = rot + b + d;
-        dp[-8] = evsum + oddrot;
-        dp[-7] = evdiff + rot;
-        dp[-6] = evdiff - rot;
-        dp[-5] = evsum - oddrot;
+        s32 odddiff0 = b0 - d0;
+        s32 oddsum0  = b0 + d0;
+        s32 evsum0   = a0 + c0;
+        s32 evdiff0  = a0 - c0;
+        s32 rot0     = (odddiff0 * 0xB5) >> 8;
 
-        sp -= 16;
-        dp -= 16;
+        s32 odddiff1 = b1 - d1;
+        s32 oddsum1  = b1 + d1;
+        s32 evsum1   = a1 + c1;
+        s32 evdiff1  = a1 - c1;
+        s32 rot1     = (odddiff1 * 0xB5) >> 8;
+
+        dst[0]   = evsum0 + (oddsum0 + rot0);
+        dst[1]   = evdiff0 + rot0;
+        dst[2]   = evdiff0 - rot0;
+        dst[3]   = evsum0 - (oddsum0 + rot0);
+
+        dst[-8]  = evsum1 + (oddsum1 + rot1);
+        dst[-7]  = evdiff1 + rot1;
+        dst[-6]  = evdiff1 - rot1;
+        dst[-5]  = evsum1 - (oddsum1 + rot1);
+
+        src -= 16;
+        dst -= 16;
     }
 
-    sp = tmp + 5;
-    for (i = 0; i < 4; i++) {
-        s32 v;
-        a = sp[0];
-        b = sp[8];
-        c = sp[16];
-        d = sp[24];
+    src = tmp + 3;
+    for (i = 3; i >= 0; i--) {
+        u8* out = conv_row_ptr + i;
+        a = src[0];
+        b = src[8];
+        c = src[16];
+        d = src[24];
+
+        odddiff = b - d;
+        oddsum = b + d;
         evsum = a + c;
         evdiff = a - c;
+        rot = odddiff * 0xB5 >> 8;
+        oddrot = oddsum + rot;
 
-        rot = (b - d) * 0xB5 >> 8;
-        oddrot = rot + b + d;
+        out[0]  = (u8)Clamp_S8((evsum + oddrot) >> 11);
+        out[8]  = (u8)Clamp_S8((evdiff + rot) >> 11);
+        out[16] = (u8)Clamp_S8((evdiff - rot) >> 11);
+        out[24] = (u8)Clamp_S8((evsum - oddrot) >> 11);
 
-        v = (evsum + oddrot) >> 11;
-        conv_row_ptr[i] = (u8)Clamp_S8(v);
-
-        v = (evdiff + rot) >> 11;
-        conv_row_ptr[i + sizeof(u8)] = (u8)Clamp_S8(v);
-
-        v = (evdiff - rot) >> 11;
-        conv_row_ptr[i + sizeof(u8) * 2] = (u8)Clamp_S8(v);
-
-        v = (evsum - oddrot) >> 11;
-        conv_row_ptr[i + sizeof(u8) * 3] = (u8)Clamp_S8(v);
-
-        sp = sp + -1;
+        src--;
     }
 }
 
